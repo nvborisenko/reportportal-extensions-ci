1c9c3d4a7a6487b012e5223a9a489afeb5608eda
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-05-08 16:33:01 +0300

    Support jenkins

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# reportportal-extensions-ci
+Automatically append build information from CI to the launch in Report Portal.
+
+# Azure DevOps
+
+## Branch name
+Takes `BRANCH_NAME` env var and set it as launch attribute.
+
+## Merge retried tests
+If you use Visual Studio Test task to execute tests, and you retried failed tests. 
+
+# Jenkins
+
+Activated only if there is `JENKINS_URL` environment variable (already set by jenkins).
+
+## Branch name
+Takes `BRANCH_NAME` env var and set it as launch attribute.
+
+## Build description
+Put the launch link into build description (requires `username`/`apiToken`). It gets jenkins host via `BUILD_URL` environment variable.
+
+Configuration
+```json
+{
+    "extensions": {
+        "ci": {
+            "jenkins": {
+                "username": "<your_jenkins_username>",
+                "apiToken": "<user_api_token>"
+            }
+        }
+    }
+}
+```
---
+++ src/ReportPortal.Extensions.CI/Providers/JenkinsProvider.cs
@@ -0,0 +1,77 @@
+﻿using ReportPortal.Client.Abstractions.Models;
+using ReportPortal.Shared.Extensibility;
+using ReportPortal.Shared.Extensibility.ReportEvents;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace ReportPortal.Extensions.CI.Providers
+{
+    public class JenkinsProvider : IReportEventsObserver
+    {
+        public void Initialize(IReportEventsSource reportEventsSource)
+        {
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("JENKINS_URL")))
+            {
+                reportEventsSource.OnBeforeLaunchStarting += ReportEventsSource_OnBeforeLaunchStarting;
+                reportEventsSource.OnAfterLaunchFinished += ReportEventsSource_OnAfterLaunchFinished;
+            }
+        }
+
+        private void ReportEventsSource_OnBeforeLaunchStarting(Shared.Reporter.ILaunchReporter launchReporter, Shared.Extensibility.ReportEvents.EventArgs.BeforeLaunchStartingEventArgs args)
+        {
+            // determine branch
+            var branchName = Environment.GetEnvironmentVariable("BRANCH_NAME");
+            if (!string.IsNullOrEmpty(branchName))
+            {
+                if (args.StartLaunchRequest.Attributes == null)
+                {
+                    args.StartLaunchRequest.Attributes = new List<ItemAttribute>();
+                }
+
+                args.StartLaunchRequest.Attributes.Add(new ItemAttribute { Key = "branch", Value = branchName });
+            }
+        }
+
+        private void ReportEventsSource_OnAfterLaunchFinished(Shared.Reporter.ILaunchReporter launchReporter, Shared.Extensibility.ReportEvents.EventArgs.AfterLaunchFinishedEventArgs args)
+        {
+            // update build description in jenkins
+            var jenkinsUsername = args.Configuration.GetValue<string>("Extensions:CI:Jenkins:Username");
+            var jenkinsApiToken = args.Configuration.GetValue<string>("Extensions:CI:Jenkins:ApiToken");
+
+            var buildUrl = Environment.GetEnvironmentVariable("BUILD_URL");
+
+            using (var httpClient = new HttpClient())
+            {
+                var base64Token = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{jenkinsUsername}:{jenkinsApiToken}"));
+
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64Token);
+
+                // getting current description
+                string oldBuildDescription;
+
+                var response = httpClient.GetStringAsync($"{buildUrl}api/json").GetAwaiter().GetResult();
+
+                oldBuildDescription = System.Text.Json.JsonDocument.Parse(response).RootElement.GetProperty("description").GetString();
+
+                string newBuildDescription;
+
+                if (string.IsNullOrEmpty(oldBuildDescription))
+                {
+                    newBuildDescription = launchReporter.Info.Url;
+                }
+                else
+                {
+                    newBuildDescription = $"{oldBuildDescription}\n\n{launchReporter.Info.Url}";
+                }
+
+                var formContent = new FormUrlEncodedContent(new[]
+                    {
+                        new KeyValuePair<string, string>("description", newBuildDescription)
+                    });
+
+                httpClient.PostAsync($"{buildUrl}submitDescription", formContent).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
