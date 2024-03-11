ce3a2b824bd75bb48ee0704eb5667f56c9dc8a32
Shakevg <42133199+Shakevg@users.noreply.github.com>
2024-03-11 14:49:45 +0500

    launch-debug-mode: fix search for launches in LaunchDebugMode, small refactoring (#5)
    Co-authored-by: Victor Grigoriev <Victor_Grigoriev@epam.com>

--- src/ReportPortal.Extensions.CI/Providers/AzureDevOpsProvider.cs
+++ src/ReportPortal.Extensions.CI/Providers/AzureDevOpsProvider.cs
@@ -1,27 +1,33 @@
-﻿using ReportPortal.Client.Abstractions.Filtering;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ReportPortal.Client.Abstractions.Filtering;
 using ReportPortal.Client.Abstractions.Models;
+using ReportPortal.Shared.Configuration;
 using ReportPortal.Shared.Extensibility;
 using ReportPortal.Shared.Extensibility.ReportEvents;
 using ReportPortal.Shared.Extensibility.ReportEvents.EventArgs;
 using ReportPortal.Shared.Reporter;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ReportPortal.Extensions.CI.Providers
 {
     public class AzureDevOpsProvider : IReportEventsObserver
     {
-        private const string KEY = "DTA.EnvironmentUri";
+        private const string CONTEXT_KEY = "DTA.EnvironmentUri";
+        private const string AZURE_BUILD_KEY = "TF_BUILD";
+        private const string VS_TEST_TASK_RERUN_KEY = "TE.RerunMaxAttempts";
+        private const string AZURE_BRANCH_NAME_KEY = "BUILD_SOURCEBRANCHNAME";
+
+        public string ContextValue { get; set; }
 
         public void Initialize(IReportEventsSource reportEventsSource)
         {
             // track context only if it's azure and rerun is set in pipeline
-            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("TF_BUILD")))
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable(AZURE_BUILD_KEY)))
             {
                 reportEventsSource.OnBeforeLaunchStarting += BranchHandler;
 
-                if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("TE.RerunMaxAttempts")))
+                if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable(VS_TEST_TASK_RERUN_KEY)))
                 {
                     reportEventsSource.OnLaunchInitializing += ReportEventsSource_OnLaunchInitializing;
 
@@ -30,55 +36,53 @@ namespace ReportPortal.Extensions.CI.Providers
             }
         }
 
-        private void BranchHandler(ILaunchReporter launchReporter, BeforeLaunchStartingEventArgs args)
+        private static void BranchHandler(ILaunchReporter launchReporter, BeforeLaunchStartingEventArgs args)
         {
             // determine branch
-            var branchName = Environment.GetEnvironmentVariable("BUILD_SOURCEBRANCHNAME");
+            var branchName = Environment.GetEnvironmentVariable(AZURE_BRANCH_NAME_KEY);
             if (!string.IsNullOrEmpty(branchName))
             {
                 if (args.StartLaunchRequest.Attributes == null)
-                {
                     args.StartLaunchRequest.Attributes = new List<ItemAttribute>();
-                }
 
                 args.StartLaunchRequest.Attributes.Add(new ItemAttribute { Key = "branch", Value = branchName });
             }
         }
 
-        public string ContextValue { get; set; }
-
-        private void ReportEventsSource_OnLaunchInitializing(ILaunchReporter launchReporter, LaunchInitializingEventArgs args)
+        private void ReportEventsSource_OnLaunchInitializing(ILaunchReporter launchReporter,
+            LaunchInitializingEventArgs args)
         {
-            ContextValue = Environment.GetEnvironmentVariable(KEY);
+            ContextValue = Environment.GetEnvironmentVariable(CONTEXT_KEY);
 
-            var filter = new FilterOption()
+            var filter = new FilterOption
             {
-                Filters = new List<Filter> {
-                    new Filter(FilterOperation.Contains, "description", ContextValue), },
+                Filters = new List<Filter>
+                {
+                    new Filter(FilterOperation.Contains, "description", ContextValue)
+                },
                 Sorting = new Sorting(new List<string> { "startTime" }, SortDirection.Descending)
             };
 
-            var launches = args.ClientService.Launch.GetAsync(filter).GetAwaiter().GetResult();
+            var launches = args.Configuration.GetValue(ConfigurationPath.LaunchDebugMode, false)
+                ? args.ClientService.Launch.GetDebugAsync(filter).GetAwaiter().GetResult()
+                : args.ClientService.Launch.GetAsync(filter).GetAwaiter().GetResult();
 
-            if (launches.Items.Count() != 0)
+            if (launches.Items.Any())
             {
                 args.Configuration.Properties["Launch:Rerun"] = true;
-                args.Configuration.Properties["Launch:RerunOf"] = launches.Items.First().Uuid;
+                args.Configuration.Properties["Launch:RerunOf"] = launches.Items[0].Uuid;
             }
         }
 
-        private void ReportEventsSource_OnBeforeLaunchStarting(ILaunchReporter launchReporter, BeforeLaunchStartingEventArgs args)
+        private void ReportEventsSource_OnBeforeLaunchStarting(ILaunchReporter launchReporter,
+            BeforeLaunchStartingEventArgs args)
         {
-            var hiddenInfo = $"<!-- {KEY}:{ContextValue} -->";
+            var hiddenInfo = $"<!-- {CONTEXT_KEY}:{ContextValue} -->";
 
             if (args.StartLaunchRequest.Description != null)
-            {
                 args.StartLaunchRequest.Description += Environment.NewLine + hiddenInfo;
-            }
             else
-            {
                 args.StartLaunchRequest.Description = hiddenInfo;
-            }
         }
     }
 }
